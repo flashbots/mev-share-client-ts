0f5839d5e738ccf7c46f736e5347cfcd4c807f15
Brock Smedley <2791467+zeroXbrock@users.noreply.github.com>
2023-03-15 14:06:57 -0700

    improve backrun example; target next 5 blocks for every backrun

--- src/examples/sendBackrunBundle.ts
+++ src/examples/sendBackrunBundle.ts
@@ -7,12 +7,14 @@ import { ShareBundleParams, PendingShareTransaction } from '../api'
 import { getProvider, initExample } from './lib/helpers'
 import { sendTx, setupTxExample } from './lib/sendTx'
 
+const NUM_TARGET_BLOCKS = 5
+
 /**
  * Generate a transaction to backrun a pending mev-share transaction and send it to mev-share.
  */
 const sendTestBackrunBundle = async (provider: JsonRpcProvider, pendingTx: PendingShareTransaction, matchmaker: Matchmaker, targetBlock: number) => {
     // send ofa bundle w/ 42 gwei priority fee
-    let {tx, wallet} = (await setupTxExample(provider, BigInt(1e9) * BigInt(42), "im backrunniiiiing"))
+    let {tx, wallet} = (await setupTxExample(provider, BigInt(1e9) * BigInt(69), "im backrunniiiiing"))
     tx = {
         ...tx,
         nonce: tx.nonce ? tx.nonce + 1 : undefined,
@@ -21,17 +23,21 @@ const sendTestBackrunBundle = async (provider: JsonRpcProvider, pendingTx: Pendi
         [await wallet.signTransaction(tx)]
     ]
     const shareTxs = [pendingTx.txHash]
-    const params: ShareBundleParams = {
-        targetBlock,
-        backruns,
-        shareTxs,
+    let backrunResults = []
+    console.log(`sending backrun bundles targeting next ${NUM_TARGET_BLOCKS} blocks...`)
+    for (let i = 0; i < NUM_TARGET_BLOCKS; i++) {
+        const params: ShareBundleParams = {
+            targetBlock: targetBlock + i,
+            backruns,
+            shareTxs,
+        }
+        const backrunRes = matchmaker.sendShareBundle(params)
+        console.debug("sent share bundle", JSON.stringify(params))
+        backrunResults.push(backrunRes)
     }
-    console.debug(JSON.stringify(params))
-    const backrunRes = await matchmaker.sendShareBundle(params)
-    console.log("backrun result", backrunRes)
     return {
         backruns,
-        backrunRes,
+        backrunResults: await Promise.all(backrunResults),
     }
 }
 
@@ -44,22 +50,32 @@ const handleBackrun = async (
 ) => {
     console.log("pending tx", pendingTx)
     let targetBlock = await provider.getBlockNumber() + 2
-    let { backruns } = await sendTestBackrunBundle(provider, pendingTx, matchmaker, targetBlock)
+    let { backruns, backrunResults } = await sendTestBackrunBundle(provider, pendingTx, matchmaker, targetBlock)
+    console.log("backrun results", backrunResults)
 
-    // block thread until target block is verified
-    while (await provider.getBlockNumber() < targetBlock) {
-        await new Promise(resolve => setTimeout(resolve, 2000))
-    }
+    // watch future blocks for backrun tx inclusion
+    for (let i = 0; i < NUM_TARGET_BLOCKS; i++) {
+        if (!pendingMutex.isLocked()) {
+            // mutex was released by another handler, so we can exit
+            break
+        }
+        console.log(`tx ${pendingTx.txHash} waiting for block`, targetBlock + i)
+        // poll until block is available
+        while (await provider.getBlockNumber() < targetBlock + i) {
+            await new Promise(resolve => setTimeout(resolve, 2000))
+        }
 
-    // check for inclusion of backrun tx in target block
-    const backrunTxHash = keccak256(backruns[0][0])
-    const receipt = await provider.getTransactionReceipt(backrunTxHash)
-    if (receipt?.status === 1) {
-        console.log("backrun tx included!")
-        // release mutex so the main thread can exit
-        pendingMutex.release()
-    } else {
-        console.warn(`tx ${backrunTxHash} not included in block ${targetBlock}`)
+        // check for inclusion of backrun tx in target block
+        const backrunTxHash = keccak256(backruns[0][0])
+        const receipt = await provider.getTransactionReceipt(backrunTxHash)
+        if (receipt?.status === 1) {
+            console.log("backrun tx included!")
+            // release mutex so the main thread can exit
+            pendingMutex.release()
+            break
+        } else {
+            console.warn(`tx ${backrunTxHash} not included in block ${targetBlock}`)
+        }
     }
 }
 
