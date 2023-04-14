9c762b424b7c0cc3666dfde92dd209c4df20938a
zeroXbrock <2791467+zeroXbrock@users.noreply.github.com>
2023-04-13 18:15:40 -0700

    remove 'auction.enable' param, set tx hash in hint munger

--- src/api/mungers.ts
+++ src/api/mungers.ts
@@ -1,35 +1,46 @@
 import { BundleParams, HintPreferences, TransactionOptions } from './interfaces'
 
+/**
+ * Convert name format of user-specified hints for Matchmaker API requests.
+ * @param hints Hints specified by the user.
+ */
 const mungeHints = (hints: HintPreferences) => {
     return {
         contract_address: hints.contractAddress,
         function_selector: hints.functionSelector,
         calldata: hints.calldata,
         logs: hints.logs,
+        transaction_hash: true, // tx hash is always shared on Flashbots Matchmaker; abstract away from user
+        // setting all hints except transaction_hash to false will enable full privacy
     }
 }
 
-const extractSpecifiedHints = (hints: HintPreferences) => {
+/**
+ * Converts user-specified hints into the array format accepted by the API.
+ * @param hints Hints specified by the user.
+ */
+const extractSpecifiedHints = (hints: HintPreferences): string[] => {
     return Object.entries(mungeHints(hints))
         .map((kv: [string, any]) => kv[1] ? kv[0] : undefined)
         .filter(v => !!v) as string[]
 }
 
-const allHintsDisabled = (hints?: HintPreferences): boolean => {
-    return hints ? Object.values(hints).reduce((prv, curr) => !prv && !curr) : false
-}
-
+/**
+ * Converts user-specified parameters into parameters for a sendPrivateTransaction call to the Matchmaker API.
+ * @param signedTx Signed transaction to send.
+ * @param options Privacy/execution settings for the transaction.
+ * @returns Single-element array containing params object for sendPrivateTransaction call.
+ */
 export function mungePrivateTxParams(signedTx: string, options?: TransactionOptions) {
-    const enable = !allHintsDisabled(options?.hints)
     return [{
         tx: signedTx,
         maxBlockNumber: options?.maxBlockNumber && `0x${options.maxBlockNumber.toString(16)}`,
         preferences: {
             fast: true, // deprecated but required; setting has no effect
-            auction: options?.hints && !allHintsDisabled(options.hints) ? {
-                enable,
-                hint: options?.hints ? extractSpecifiedHints(options.hints) : undefined,
-            } : undefined,
+            // auction uses default (Stable) config if no hints specified
+            auction: options?.hints && {
+                hint: extractSpecifiedHints(options.hints),
+            },
         },
     }]
 }
@@ -41,9 +52,16 @@ export function mungePrivateTxParams(signedTx: string, options?: TransactionOpti
 //     }]
 // }
 
+/**
+ * Converts user-specified parameters into parameters for a mev_sendBundle call to the Matchmaker API.
+ * @param params Privacy/execution parameters for the bundle
+ * @returns Single-element array containing params object for sendPrivateTransaction call.
+ */
 export function mungeSendBundleParams(params: BundleParams) {
+    const body = params.body.map(tx => ({...tx, can_revert: 'canRevert' in tx ? tx.canRevert : undefined}))
     return [{
         ...params,
         inclusion: {...params.inclusion, block: `0x${params.inclusion.block.toString(16)}`},
+        body,
     }]
 }
