1dea795afc5d2569e9382a3ce4ff76c29b010293
zeroXbrock <2791467+zeroXbrock@users.noreply.github.com>
2023-04-17 14:50:39 -0700

    fix mungers

--- src/api/mungers.ts
+++ src/api/mungers.ts
@@ -4,14 +4,14 @@ import { BundleParams, HintPreferences, TransactionOptions } from './interfaces'
  * Convert name format of user-specified hints for Matchmaker API requests.
  * @param hints Hints specified by the user.
  */
-const mungeHints = (hints: HintPreferences) => {
+const mungeHintPreferences = (hints: HintPreferences) => {
     return {
         contract_address: hints.contractAddress,
         function_selector: hints.functionSelector,
         calldata: hints.calldata,
         logs: hints.logs,
-        transaction_hash: true, // tx hash is always shared on Flashbots Matchmaker; abstract away from user
-        // setting all hints except transaction_hash to false will enable full privacy
+        hash: true, // tx hash is always shared on Flashbots Matchmaker; abstract away from user
+        // setting all hints except hash to false will enable full privacy
     }
 }
 
@@ -20,7 +20,7 @@ const mungeHints = (hints: HintPreferences) => {
  * @param hints Hints specified by the user.
  */
 const extractSpecifiedHints = (hints: HintPreferences): string[] => {
-    return Object.entries(mungeHints(hints))
+    return Object.entries(mungeHintPreferences(hints))
         .map((kv: [string, any]) => kv[1] ? kv[0] : undefined)
         .filter(v => !!v) as string[]
 }
@@ -51,15 +51,16 @@ export function mungePrivateTxParams(signedTx: string, options?: TransactionOpti
  * @returns Single-element array containing params object for sendPrivateTransaction call.
  */
 export function mungeSendBundleParams(params: BundleParams) {
-    const body = params.body.map(tx => ({...tx, can_revert: 'canRevert' in tx ? tx.canRevert : undefined}))
     return [{
         ...params,
-        body,
-        inclusion: {...params.inclusion, block: `0x${params.inclusion.block.toString(16)}`},
+        inclusion: {
+            ...params.inclusion,
+            block: `0x${params.inclusion.block.toString(16)}`,
+            maxBlock: params.inclusion.maxBlock ? `0x${params.inclusion.maxBlock.toString(16)}` : undefined,
+        },
         privacy: {
             ...params.privacy,
             hints: extractSpecifiedHints(params.privacy.hints),
-            target_builders: params.privacy.targetBuilders,
         }
     }]
 }
