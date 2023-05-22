924018ad92f0ea1750b6af712f0e82a5276b809f
zeroXbrock <2791467+zeroXbrock@users.noreply.github.com>
2023-05-22 11:20:37 -0700

    add doc-comments for AsyncArray

--- src/examples/lib/async.ts
+++ src/examples/lib/async.ts
@@ -30,23 +30,27 @@ export class AsyncArray<T> extends AsyncVar<Array<T>> {
         super(value || [])
     }
 
+    /** Pushes a new item to the array. */
     public async push(value: T): Promise<void> {
         const arr = await this.get()
         arr.push(value)
         await this.set(arr)
     }
 
+    /** Filter array in-place. */
     public async filter(filterFn: (value: T) => boolean): Promise<void> {
         const arr = await this.get()
         const filtered = arr.filter(filterFn)
         await this.set(filtered)
     }
 
+    /** Returns true if value is included in the array. */
     public async includes(value: T): Promise<boolean> {
         const arr = await this.get()
         return arr.includes(value)
     }
 
+    /** Returns length of array. */
     public async length(): Promise<number> {
         const arr = await this.get()
         return arr.length
