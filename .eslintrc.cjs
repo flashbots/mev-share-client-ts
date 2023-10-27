fc6a027d39d94225c6e857af1faebc44de2e1176
deiay <davidaideyan@gmail.com>
2023-10-27 12:44:50 -0400

    add refund option

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,7 +2,7 @@ module.exports = {
     extends: ['plugin:@typescript-eslint/recommended'],
     parser: '@typescript-eslint/parser',
     parserOptions: {
-        project: "./tsconfig.json",
+        project: './tsconfig.json',
         tsconfigRootDir: __dirname,
         emcaVersion: 2018,
         sourceType: 'module',
@@ -11,11 +11,24 @@ module.exports = {
     root: true,
     rules: {
         '@typescript-eslint/no-explicit-any': 'off',
-        "tsdoc/syntax": "warn",
-        "require-jsdoc": "warn",
-        "jsdoc/check-tag-names": "off",
-        "jsdoc/require-param-type": "off",
-        "jsdoc/require-returns-type": "off",
-        "jsdoc/empty-tags": "off"
-    }
-};
+        '@typescript-eslint/member-delimiter-style': [
+            'warn',
+            {
+                multiline: {
+                    delimiter: 'comma',
+                    requireLast: true,
+                },
+                singleline: {
+                    delimiter: 'comma',
+                    requireLast: false,
+                },
+            },
+        ],
+        'tsdoc/syntax': 'warn',
+        'require-jsdoc': 'warn',
+        'jsdoc/check-tag-names': 'off',
+        'jsdoc/require-param-type': 'off',
+        'jsdoc/require-returns-type': 'off',
+        'jsdoc/empty-tags': 'off',
+    },
+}
